049cda7478ceb56eb7576f2255eef277ab3d211b
Gustavo Alves Ramos <0001138233@senaimgaluno.com.br>
2026-04-09 14:57:24 -0300

    Novos arquivos adicionados

--- teste_git/src/main/java/org/example/main.java
+++ teste_git/src/main/java/org/example/main.java
@@ -1,16 +1,18 @@
-import java.util.Scanner; // 1. Importando a classe Scanner
+package org.example;
 
-public class Main {
+import java.util.Scanner;
+
+public class main {
 
     public static void main(String[] args) {
-    //comentário
+
         Scanner leitor = new Scanner(System.in);
         int numero1, numero2;
         System.out.println("Digite seu nome: ");
         String nome = leitor.nextLine();
 
-        for(int i = 0;  i < 3; i++){
-            System.out.println("Seasdasu nome i :" + "Matheus");
+        {
+            System.out.println("Ola seu nome é :" + nome);
         }
         System.out.println("========== Escolha as Opções a Baixo: ==========");
         System.out.println("1 - Para Soma \n2 - Para Subtração \n3 - Para Sair");
@@ -23,7 +25,7 @@ public class Main {
                 System.out.print("Digite o segundo número: ");
                 numero2 = leitor.nextInt();
 
-                int soma = numero1 * numero2;
+                int soma = numero1 + numero2;
 
                 System.out.println("A soma de " + numero1 + " e " + numero2 + " é: " + soma);
                 break;
@@ -34,18 +36,20 @@ public class Main {
                 System.out.print("Digite o segundo número: ");
                 numero2 = leitor.nextInt();
 
-                float subtracao = numero1 / numero2;
+                float subtracao = numero1 - numero2;
 
                 System.out.println("A subtração de " + numero1 + " e " + numero2 + " é: " + subtracao);
                 break;
             case 3:
-                for(int i = 0;  i < 3; i++){
-                    System.out.println("Fiz um show SoldOut:" + "Paypal");
-                    System.out.println("saindo do Teto");
-                }
+                System.out.println("Sitema finalizado");
+                leitor.close();
                 break;
+
+            default:
+                System.out.println("Opção inválida! Escolha apenas 1, 2 ou 3.");
+                break;
+
         }
-        leitor.close();
     }
     
 }
